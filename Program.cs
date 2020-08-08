cd5510dfa1645e1bece29e56b87918a9d887d993
Henrique Akao <henriquetanioka@gmail.com>
2020-08-07 21:26:12 -0300

    v1

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using ex12.Entities;
+
+namespace ex12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                switch (ch)
+                {
+                    case 'c':
+                        list.Add(new Product(name, price));
+                        break;
+                    case 'u':
+                        Console.Write("Manufacture date (DD/MM/YYYY): ");
+                        DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                        list.Add(new UsedProduct(name, price, manufactureDate));
+                        break;
+                    case 'i':
+                        Console.Write("Customs fee: ");
+                        double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        list.Add(new ImportedProduct(name, price, customsFee));
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p.PriceTag());
+            }
+        }
+    }
+}
